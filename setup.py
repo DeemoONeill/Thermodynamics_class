778b1f2b76d222529085c419fc6aea2280bf3147
Matthew O'Neill <oneill_91@hotmail.co.uk>
2019-02-15 22:31:15 +0000

    added setup.py and created a distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 22:17:01 2019
+
+@author: oneil
+"""
+
+from distutils.core import setup
+
+setup(name='Thermodynamics',
+      version='1.0',
+      py_modules=['Thermodynamics'],
+      )
