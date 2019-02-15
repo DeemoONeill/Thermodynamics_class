ea4c9c8b563283699dde5b1b394841de456d7528
Matthew O'Neill <oneill_91@hotmail.co.uk>
2019-02-15 23:04:53 +0000

    updated setup.py, added dist

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@
 """
 Created on Fri Feb 15 22:17:01 2019
 
-@author: oneil
+@author: Matthew O'Neill
 """
 
 from distutils.core import setup
 
 setup(name='Thermodynamics',
-      version='1.0',
+      version='0.9.0',
+      description='Python Thermodynamic Equilibrium Position Calculator',
+      author="Matthew O'Neill",
+      download_url='https://github.com/DeemoONeill/Thermodynamics_class/\
+      tree/master',
       py_modules=['Thermodynamics'],
       )
